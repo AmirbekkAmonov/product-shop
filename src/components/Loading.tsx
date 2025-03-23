6d884ebcdfba1652c0ae3345d64704f440899b3a
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-03-23 16:38:59 +0500

    loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,8 +4,11 @@ import '@/styles/loading.scss';
 const Loading = () => {
   return (
     <div className="loading">
-      <div className="loading__spinner"></div>
-      <p className="loading__text">Yuklanmoqda...</p>
+      <div className="loading__container">
+        <div className="loading__circle"></div>
+        <div className="loading__circle"></div>
+        <div className="loading__circle"></div>
+      </div>
     </div>
   );
 };
